6f20c617ec64e10568324ce7e51ad50df62ee452
Praveen Poonia <Praveen.Poonia@akqa.com>
2019-03-02 07:40:53 +0530

    Updates

---
+++ precache-manifest.9aeb08daacbd9b67e0fc86d9f3e278c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f24e6433687153c4d3040a2632aaf59",
+    "url": "/jobs.github-with-react/static/media/galaxy.9f24e643.png"
+  },
+  {
+    "revision": "d59eaf1f390817353927",
+    "url": "/jobs.github-with-react/static/js/runtime~main.d59eaf1f.js"
+  },
+  {
+    "revision": "93f839546531a6f4e70b",
+    "url": "/jobs.github-with-react/static/js/main.93f83954.chunk.js"
+  },
+  {
+    "revision": "5bb62acd97ad4a52be18",
+    "url": "/jobs.github-with-react/static/js/2.5bb62acd.chunk.js"
+  },
+  {
+    "revision": "93f839546531a6f4e70b",
+    "url": "/jobs.github-with-react/static/css/main.b0f55940.chunk.css"
+  },
+  {
+    "revision": "2ecc0bd8da66948b4a45e4744580fbb5",
+    "url": "/jobs.github-with-react/index.html"
+  }
+];
